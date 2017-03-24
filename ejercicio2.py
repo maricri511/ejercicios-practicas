f3a5c342ba8a9e1639c3ce7f8640a07caf656c73
maria cristina ramos <maria cristina ramos>
2017-03-24 12:14:42 -0300

    Agrego ejercicio2.

---
+++ ejercicio2.py
@@ -0,0 +1,9 @@
+numerobcos = 25
+
+alumnos = int(input("ingrese numero de alumnos: "))
+if (alumnos > numerobcos):
+    dif = alumnos - numerobcos
+    print("faltan" + str(dif) + "bancos")
+else
+    print("los alumnos se pueden inscribir")
+    
