58ae601e89b8b58c4f25a96b155f293baf1ab2eb
maria cristina ramos <maria cristina ramos>
2017-03-29 15:33:29 -0300

    agrego ejrcicio 17

--- ejercicio17.py
+++ ejercicio17.py
@@ -1,3 +1,9 @@
+#i = 0
+#for i in range(9):
+#        print("_" * i, "|")
 i = 0
-for i in range(9):
-        print("_" * i, "|")
+renglon = 0
+while (renglon < 10):
+    print("_" * renglon, "|")
+    renglon += 1
+    
