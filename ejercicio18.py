f166f02b8e9cf9efc929f4ddffb12b240ed9e23d
maria cristina ramos <maria cristina ramos>
2017-04-01 00:40:40 -0300

    Agrego ejercicio 18

---
+++ ejercicio18.py
@@ -0,0 +1,8 @@
+cond = 0
+print("T","C", "A", "M", "R", "A", "C", "T")
+print("P", "P", "P", "P", "P", "P", "P", "P",)
+while (cond < 4):
+    print(" " * 16)
+    cond += 1
+print("P", "P", "P", "P", "P", "P", "P", "P")
+print("T", "C", "A", "M", "R", "A", "C", "T")
